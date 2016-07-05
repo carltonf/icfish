3e245f5baf894e4d744354bd5750bcc52124ed21
Carl Xiong <xiongc05@gmail.com>
2016-07-05 16:15:52 +0800

    feat(init): my first fish configuration
    * NOTE for convenience, 'bass' has been included in the repo directly.

---
+++ config.fish
@@ -0,0 +1,26 @@
+# Use the same aliases as Bash
+source ~/.bash_aliases
+
+#### environment vars settings
+# bin: icrepos binaries;
+#
+# xbin: mainly links or tools not meant for repo. To facilitate easily adding
+# new binary path, add the first-level subdirectories.
+set -x PATH $HOME/local/xbin $HOME/local/bin $PATH
+for d in $HOME/local/xbin/*/
+  if test -d $d
+    set -x PATH $d $PATH
+  end
+end
+
+# Enable gpg ssh emulation
+# TODO is this still necessary
+set -x SSH_AUTH_SOCK $HOME/.gnupg/S.gpg-agent.ssh
+
+# NOTE 'vi' has been linked to 'neovim'
+set -x EDITOR /usr/bin/vi
+
+# NOTE: `bass' has resolved the whole `nvm' issue completely.
+
+# NOTE `RVM' and Ruby stuff is not used here for now, the same for Emacs `Cask',
+# which might not be used anymore.
---
+++ functions/fish_reinit.fish
@@ -0,0 +1,3 @@
+function fish_reinit
+	source $HOME/.config/fish/config.fish
+end
