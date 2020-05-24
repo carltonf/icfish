03857288ebe7eeb0ad9d8333e0dd3a07c35cbc71
Carl Xiong <xiongc05@gmail.com>
2020-05-24 14:06:11 +0000

    prompt: incorporating python venv prompt
    * Also provide extra flexibility for adding more custom prompts

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -16,14 +16,36 @@ function __fish_prompt_status -d 'indicating the current status (default to host
   echo (hostname)
 end
 
+function __fish_printf_status -a prt -d 'printf status only when there is output, avoid excessive whitespace'
+  if test -n "$prt"
+    printf '%s ' "$prt"
+  else
+    printf ''
+  end
+end
+
+function __fish_extra_status -d 'overriding this function to show extra status'
+  # python venv
+  # Also add this following to $VIRTUAL_ENV/bin/activate.fish
+  # set -x VIRTUAL_ENV_DISABLE_PROMPT y
+  if test -n "$VIRTUAL_ENV"
+    printf "%s(%s)%s" (set_color -b blue white) (basename "$VIRTUAL_ENV") (set_color normal)
+  else
+    printf ''
+  end 
+end
+
 function fish_prompt
   set last_status $status
-  set_color blue
-  printf '%s: ' (__fish_prompt_status)
+  printf '%s\n' (printf %"$COLUMNS"s | tr " " "_")
+  printf '[%s %s] ' (date +%X) (date +%x)
   set_color $fish_color_cwd
-  printf '%s' (prompt_pwd)
+  printf '%s\n' (pwd)
+  set_color blue
+  printf '[%s] ' (__fish_prompt_status)
+  set_color cyan
+  __fish_printf_status (__fish_extra_status)
   set_color normal
-  printf '%s ' (__fish_git_prompt)
-  set_color normal
+  __fish_printf_status (__fish_git_prompt)
   printf '$ '
 end
