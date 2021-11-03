b05af34c228a12534b5c518c9cce81279f483e45
Carl Xiong <xiongc05@gmail.com>
2021-11-03 10:40:40 +0000

    extra status

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -24,16 +24,50 @@ function __fish_printf_status -a prt -d 'printf status only when there is output
   end
 end
 
-function __fish_extra_status -d 'overriding this function to show extra status'
-  # python venv
-  # Also add this following to $VIRTUAL_ENV/bin/activate.fish
-  # set -x VIRTUAL_ENV_DISABLE_PROMPT y
-  if test -n "$VIRTUAL_ENV"
-    printf "%s(%s)%s" (set_color -b blue white) (basename "$VIRTUAL_ENV") (set_color normal)
-  else
-    printf ''
-  end 
+# function __fish_extra_status -d 'overriding this function to show extra status'
+#   # python venv
+#   # Also add this following to $VIRTUAL_ENV/bin/activate.fish
+#   # set -x VIRTUAL_ENV_DISABLE_PROMPT y
+#   if test -n "$VIRTUAL_ENV"
+#     printf "%s(%s)%s" (set_color -b blue white) (basename "$VIRTUAL_ENV") (set_color normal)
+#   else
+#     printf ''
+#   end 
+#   if test -n "$ANSIBLE_INVENTORY_TYPE"
+#     printf "(%s)" (basename "$ANSIBLE_INVENTORY_TYPE")
+#   else
+#     printf ''
+#   end 
+#   if test -n "$GCLOUD_INIT_PROMPT"
+#     printf "%s[%s]%s" (set_color -b white red) (_gcloud_get_remaining_time) (set_color normal)
+#   else
+#     printf ''
+#   end 
+# end
+
+set -x __fish_extra_status_prompter_list
+function fish_register_status_prompter -d 'allow external programs to register extra status prompter'
+  # avoid duplications
+  for prompter in $__fish_extra_status_prompter_list
+    if test $prompter = $argv
+      return 0
+    end
+  end
+
+  set -x __fish_extra_status_prompter_list $__fish_extra_status_prompter_list $argv
 end
+function __fish_extra_status -d 'overriding this function to show extra status'
+  for prompter in $__fish_extra_status_prompter_list
+    eval $prompter
+  end
+end
+
+function __fish_tmux_remotes_status -d 'show a flag to indicate the shell is currently inside a tmux remotes session'
+  if echo "$TMUX" | grep -q 'remotes'
+    printf "(tmux-remotes)"
+  end
+end
+fish_register_status_prompter __fish_tmux_remotes_status
 
 function fish_prompt
   set last_status $status
